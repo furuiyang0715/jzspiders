0293a1fa7ae2fac1c3440bb8900203513b656b6d
furuiyang <furuiyang0715@gmail.com>
2020-09-07 17:42:33 +0800

    Little

--- JfInfo/jfinfo_main.py
+++ JfInfo/jfinfo_main.py
@@ -6,9 +6,11 @@ file_path = os.path.abspath(os.path.join(cur_path, ".."))
 sys.path.insert(0, file_path)
 
 from JfInfo.reference import HKInfo, Reference, Research, TZZJY
+from base_spider import SpiderBase
+from scripts import utils
 
 
-class JFSchedule(object):
+class JFSchedule(SpiderBase):
     """巨丰财经 爬虫调度"""
     class_lst = [
         HKInfo,  # 港股资讯
@@ -19,7 +21,10 @@ class JFSchedule(object):
 
     table_name = 'jfinfo'
     # dt_benchmark = 'pub_date'
-    name = '巨丰财经'
+    # name = '巨丰财经'
+
+    info = utils.org_tablecode_map.get(table_name)
+    name, table_code = info[0], info[1]
 
     def ins_start(self, instance):
         instance.start()
@@ -30,6 +35,29 @@ class JFSchedule(object):
             print(f"巨丰财经 --> {ins.name}")
             ins.start()
 
+    def trans_history(self):
+        self._spider_init()
+        for i in range(1000):  # TODO
+            trans_sql = '''select pub_date as PubDatetime,\
+title as Title,\
+link as Website,\
+article as Content, \
+CREATETIMEJZ as CreateTime, \
+UPDATETIMEJZ as UpdateTime \
+from {} limit {}, 1000; '''.format(self.table_name, i * 1000)
+            datas = self.spider_client.select_all(trans_sql)
+            print(len(datas))
+            if not datas:
+                break
+            for data in datas:
+                data['DupField'] = "{}_{}".format(self.table_code, data['Website'])
+                data['MedName'] = self.name
+                data['OrgMedName'] = self.name
+                data['OrgTableCode'] = self.table_code
+                self._save(self.spider_client, data, 'OriginSpiderAll', self.merge_fields)
+
 
 if __name__ == "__main__":
-    JFSchedule().start()
+    # JFSchedule().start()
+
+    JFSchedule().trans_history()
