0143c2c8772a2c0c1704a952a3fa245778b36be2
furuiyang <furuiyang0715@gmail.com>
2020-09-08 13:51:05 +0800

    bug fixed

--- notes.py
+++ notes.py
@@ -1,8 +1,25 @@
 '''转移数据
 sudo docker build -f Dockerfile -t registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/jzspi:v2 .
 
+# local
 sudo docker run -itd --name trans_history registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/jzspi:v2 /bin/bash
 
+# remote
+sudo docker run -itd --name trans_history --env LOCAL=0 registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/jzspi:v2 /bin/bash
+
 docker exec -it trans_history /bin/bash
+
+'''
+
+
+'''暂时未进行转移
+CArticle
+
+GovSpiders 
+
+Taoguba
+
+
+
 
 '''
