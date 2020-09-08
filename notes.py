5bc731b9ad1e0945f727ea97e8046afb1004e778
furuiyang <furuiyang0715@gmail.com>
2020-09-08 15:58:10 +0800

    Little

--- notes.py
+++ notes.py
@@ -10,6 +10,10 @@ sudo docker run -itd --name trans_history registry.cn-shenzhen.aliyuncs.com/jzde
 
 # remote
 sudo docker run -itd --name trans_history --env LOCAL=0 registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/jzspi:v2 /bin/bash
+# 对于数据量比较大的东财以及淘股吧
+sudo docker run -itd --name dc --env LOCAL=0 registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/jzspi:v2 python CArticle/ca_main.py
+sudo docker run -itd --name tgb --env LOCAL=0 registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/jzspi:v2 python Taoguba/tgb_main.py
+
 
 docker exec -it trans_history /bin/bash
 
