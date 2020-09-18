efe525bdb1316026a5ecb088bf6e6e36270561d2
furuiyang <furuiyang0715@gmail.com>
2020-09-18 14:13:37 +0800

    部署 selenium

--- Taoguba/sele_login.py
+++ Taoguba/sele_login.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 
-# browser = webdriver.Chrome()
+browser = webdriver.Chrome()
 
-browser = webdriver.Remote(
-    command_executor='http://127.0.0.1:32768/wd/hub',
-    desired_capabilities={'browserName': 'chrome'},
-)
+# browser = webdriver.Remote(
+#     command_executor='http://127.0.0.1:32768/wd/hub',
+#     desired_capabilities={'browserName': 'chrome'},
+# )
 browser.get('http://www.baidu.com/')
 text = browser.page_source
 print(text)
